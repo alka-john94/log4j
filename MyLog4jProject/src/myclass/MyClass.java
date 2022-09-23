886d8b0d61d7508d4668c1bc13bd10232e50b256
alka-john94 <alka.john@pagematics.in>
2022-09-23 16:36:44 +0530

    Alka: committing log4j project

---
+++ MyLog4jProject/src/myclass/MyClass.java
@@ -0,0 +1,17 @@
+package myclass;
+
+import org.apache.log4j.Logger;  
+  
+import java.io.*;  
+import java.sql.SQLException;  
+  
+public class MyClass{  
+  
+   /* Get the class name to be printed on */  
+   static Logger log = Logger.getLogger(MyClass.class.getName());  
+     
+   public static void main(String[] args)throws IOException,SQLException{  
+      log.debug("Hello this is a debug message");  
+      log.info("Hello this is an info message");  
+   }  
+}
